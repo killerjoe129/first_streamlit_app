c869ecff3e6366a72807e34d7f20c6ed672f3f20
killerjoe129 <149458177+killerjoe129@users.noreply.github.com>
2023-10-31 13:19:00 +0700

    Update streamlit_app.py
    change items and add emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
-streamlit.title('My first Streamlit')
+streamlit.title('3 Meals a Day')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
